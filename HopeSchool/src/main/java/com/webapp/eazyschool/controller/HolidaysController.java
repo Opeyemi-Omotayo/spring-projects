e5393de42a9c5f66b2180e1703dc804e04dfa244
Opeyemi Omotayo <96225514+Opeyemi-Omotayo@users.noreply.github.com>
2026-01-30 16:47:22 +0100

    migrated to mysql

--- HopeSchool/src/main/java/com/webapp/eazyschool/controller/HolidaysController.java
+++ HopeSchool/src/main/java/com/webapp/eazyschool/controller/HolidaysController.java
@@ -1,14 +1,14 @@
 package com.webapp.eazyschool.controller;
 
 import com.webapp.eazyschool.model.Holiday;
+import com.webapp.eazyschool.repository.HolidaysRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,32 +16,20 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidaysController {
 
-    /*@GetMapping("/holidays")
-    public String displayHolidays(@RequestParam(required = false) boolean festival, @RequestParam(required = false) boolean federal, Model model) {
-        model.addAttribute("festival", festival);
-        model.addAttribute("federal", federal);*/
+    @Autowired
+    private HolidaysRepository holidaysRepository;
 
-        @GetMapping("/holidays/{display}")
-        public String displayHolidays(@PathVariable String display, Model model) {
-            if(null != display && display.equals("all")){
-                model.addAttribute("festival",true);
-                model.addAttribute("federal",true);
-            }else if(null != display && display.equals("federal")){
-                model.addAttribute("federal",true);
-            }else if(null != display && display.equals("festival")){
-                model.addAttribute("festival",true);
-            }
-
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display,Model model) {
+        if(null != display && display.equals("all")){
+            model.addAttribute("festival",true);
+            model.addAttribute("federal",true);
+        }else if(null != display && display.equals("federal")){
+            model.addAttribute("federal",true);
+        }else if(null != display && display.equals("festival")){
+            model.addAttribute("festival",true);
+        }
+        List<Holiday> holidays = holidaysRepository.findAllHolidays();
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
@@ -49,4 +37,5 @@ public class HolidaysController {
         }
         return "holidays.html";
     }
+
 }
